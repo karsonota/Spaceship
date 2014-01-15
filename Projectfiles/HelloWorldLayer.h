cc91d01ad91da0416b3ebbbe1fb38894aae87a4f
Karson Ota <klota@mit.edu>
2014-01-15 14:12:09 -0500

    first commit

---
+++ Projectfiles/HelloWorldLayer.h
@@ -0,0 +1,17 @@
+/*
+ * Kobold2D™ --- http://www.kobold2d.org
+ *
+ * Copyright (c) 2010-2011 Steffen Itterheim. 
+ * Released under MIT License in Germany (LICENSE-Kobold2D.txt).
+ */
+
+#import "kobold2d.h"
+
+@interface HelloWorldLayer : CCLayer
+{
+    CCAction *spinningCoin;
+    NSMutableArray* spinningFrames;
+    
+}
+
+@end
